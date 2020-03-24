5da2fdd9b1fbbff1696b3b94a82cfc3bb32d7c2d
PhoneticSearch <dorredlich>
2020-03-24 10:30:10 +0200

    addName

--- Test.cpp
+++ Test.cpp
@@ -1,3 +1,13 @@
+/**
+ * An example of how to write unit tests.
+ * Use this as a basis to build a more complete Test.cpp file.
+ * 
+ * IMPORTANT: Please write more tests - the tests here are only for example and are not complete.
+ *
+ * AUTHORS: <Dor Redlich>
+ * 
+ * Date: 2020-02
+ */
 #include "doctest.h"
 #include "PhoneticFinder.hpp"
 using namespace phonetic;
